59ec4128a5bb2fed5f309f8cc4ab27646c86acc9
Tanel Prikk <tanel.prikk@outlook.com>
2017-04-08 19:09:23 +0300

    sendFile fix attempt

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 var express = require('express');
 var app = express();
 
-app.use(express.static(__dirname + '/'));
+//app.use(express.static(__dirname + '/'));
 app.get('/*', function (req, res) {
-    res.sendFile('index.html');
+    res.sendFile(__dirname + '/index.html');
 });
 
 var port = process.env.PORT || 8080;

2d740f517ae19243f30d7cf9117532088b4cc850
Tanel Prikk <tanel.prikk@outlook.com>
2017-04-08 19:12:51 +0300

    another attempt

--- server.js
+++ server.js
@@ -2,10 +2,14 @@ var express = require('express');
 var app = express();
 
 //app.use(express.static(__dirname + '/'));
+/*
 app.get('/*', function (req, res) {
     res.sendFile(__dirname + '/index.html');
 });
-
+*/
+app.use('*', function(req, res, next) {
+    res.sendFile(__dirname + '/index.html');
+});
 var port = process.env.PORT || 8080;
 app.listen(port);
 console.log(`listening on port: ${port}`)

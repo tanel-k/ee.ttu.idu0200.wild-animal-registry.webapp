6f962557526f9f3f6f9160c383ee1f647de82aa2
Tanel Prikk <tanel.prikk@outlook.com>
2017-04-08 19:17:24 +0300

    fix prev

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var fallback = require('express-history-api-fallback');
 
 var app = express();
 var root = `${__dirname}/`;
-app.use(express.static.root);
+app.use(express.static(root));
 app.use(fallback('index.html', { root }));
 
 var port = process.env.PORT || 8080;
